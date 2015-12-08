8450f316d396dfa0d6f5bd7ef53b5b89e91e61b5
Mattias <hemmingsson.m@gmail.com>
2015-12-08 10:23:06 +0100

    colorz

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+jQuery(document).ready(function($) {
+	setInterval(function(){ 
+		color_change() 
+	}, 1000);
+});
 
+
+
+function color_change() {
+	dark = randomColor({luminosity: 'dark'});
+	$("body").css({"border-color": dark});
+	$("body").css({"color": dark});
+}
