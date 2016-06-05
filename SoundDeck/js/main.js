1b7991ef7238ad8a41be6e30fabf743b2fd661ca
Mattias Hemmingsson <hemmingsson.m@gmail.com>
2016-06-05 18:13:11 +0200

    player improvements

--- SoundDeck/js/main.js
+++ SoundDeck/js/main.js
@@ -2,6 +2,7 @@
 var clientId = '3704e007c260afa92227c9006034af90';
 var accounts = ["moody-official-1", "svensk-rap", "clubberia", "rinsefm", "platform", "studiobarnhus", "timsweeney", "mrtophat", "babastiltz", "tjorvens", "paulahalldin", "viktor-rohlin", "kornel"]
 var audioPlayer = new Audio();
+//audioPlayer.volume = 0
 
 $(function() {
 
@@ -47,8 +48,6 @@ $(function() {
         var trackCount = data[1]
         var userId = column_element.attr('data-userid');
 
-        console.log(column_element, trackCount, userId);
-
         var tracksEndpoint = "/tracks"
 
         if (trackCount < 3) {
@@ -72,7 +71,6 @@ $(function() {
     };
 
     var renderColumn = function(userData) {
-        console.log(userData)
         userData.backgroundColor = colorByHashCode(userData.username)
         var columnElement = $(Mustache.render(column_template, userData));
         //$(".column_scroll").append(columnElement)
@@ -109,7 +107,7 @@ $(function() {
     $.each(shuffleArray(accounts), function(index, username) {
 
     });
-
+    var me = ["tjorvens"]
     var eachAccount = function() {
         if (accounts[0]) {
             new userFeed(accounts[0])
@@ -138,14 +136,19 @@ $(function() {
             return false;
         }
     });
-
+    var player_wrap = $(".player_wrap")
+    
+    var progressbar_seek = $(".progressbar_seek")
     var progressbar = $(".progressbar")
+    var time_tooltip = $("#time_tooltip")
     var progressbar_progress = $(".progressbar_progress")
     var progressbar_buffer = $(".progressbar_buffer")
     var time_elapsed = $("#time_elapsed")
     var time_total = $("#time_total")
     var track_title = $("#track_title")
     var track_owner = $("#track_owner")
+
+
 
     var updatePlayer = function() {
         var progressbar_progress_width = convert_range(audioPlayer.currentTime, [0, audioPlayer.duration], [0, progressbar.width()])
@@ -161,36 +164,81 @@ $(function() {
             $(".play_btn").removeClass('is_paused')
         } else {
             $(".play_btn").addClass('is_paused')
-            playerLoading(false)
         }
     }
 
     $(audioPlayer).bind('timeupdate', updatePlayer);
 
-    var playerLoading = function(loading) {
-        if(loading){
-            time_elapsed.addClass('flash')
-        } else{
-            time_elapsed.removeClass('flash')
-        }
+
+
+    var test = function(loading) {
+
+        console.log("sdsdssd");
     }
 
-
-    $(audioPlayer).bind('oncanplay', playerLoading(false));
-
-
+    
+    
+    //$(audioPlayer).bind('playing', playerLoading(false));
 
     var getPercentProg = function() {
         var endBuf = audioPlayer.buffered.end(0);
         var progressbar_buffer_width = parseInt(((endBuf / audioPlayer.duration) * 100));
         progressbar_buffer.width(progressbar_buffer_width + "%")
+        
     }
 
     audioPlayer.addEventListener('progress', getPercentProg, false);
 
 
+    var old_time = 0
+    setInterval(function(){
+        var current_time = parseInt(audioPlayer.currentTime)
+        if(old_time === current_time && audioPlayer.src && !audioPlayer.paused){
+            time_elapsed.addClass('flash')
+            time_tooltip.addClass('flash')
+            
+
+        } else{
+            old_time = current_time
+            time_elapsed.removeClass('flash')
+            time_tooltip.removeClass('flash')
+            
+        }
+    }, 1000);
+
+
+
+    var seek_element = $(".progressbar_seek")
+    var seekOffset = seek_element.position()
+    var mouseTime = function(e) {
+        var relX = e.pageX - seekOffset.left
+        return convert_range(relX, [0, seek_element.width()], [0, audioPlayer.duration])
+    }
+
+    seek_element.mousemove(function(e){
+        if(audioPlayer.duration){
+        var ra = mouseTime(e)
+        var human = millisecondsToHuman(ra * 1000)
+        $('#time_tooltip').css({
+           left:  e.pageX - seekOffset.left
+        });
+        $('#time_tooltip').text(human)
+        }
+        
+    }).click(function(e){
+        
+        var ra = mouseTime(e)
+        audioPlayer.currentTime = ra 
+    
+        
+    });
+    progressbar_seek.hover(function(){
+        if(audioPlayer.duration){player_wrap.addClass('seek_active')}
+        }, function(){
+        if(audioPlayer.duration){player_wrap.removeClass('seek_active')}
+    });
+
     $(document).on('click', '.play_btn', function() {
-        playerLoading()
         if (audioPlayer.paused) {
             audioPlayer.play()
         } else {
@@ -204,7 +252,6 @@ $(function() {
     }
 
     $(document).on('click', '.track', function() {
-        playerLoading(true)
         $(".play_btn").removeClass('is_paused')
         resetPlayer()
         var parentFeed = $(this).parents(".column")
